85be275010f5e0eec2a8a938d6e96113fde4bafb
WooYoung <wymoon2690@gmail.com>
2022-11-25 01:31:33 +0900

    초기

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Test>();
+
+public class Test
+{
+	Random _Random = new Random();
+
+	[BenchmarkDotNet.Attributes.Benchmark]
+	public double MethodIf()
+	{
+		return double.MaxValue % 2;
+	}
+}
