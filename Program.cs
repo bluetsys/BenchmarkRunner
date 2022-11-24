e5b876a36d04bc18d32baba6fe54e5804b581ffd
WooYoung <wymoon2690@gmail.com>
2022-11-25 01:40:01 +0900

    ㅈ

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 Console.WriteLine("Hello, World!");
 var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Test>();
 
+[BenchmarkDotNet.Attributes.SimpleJob(BenchmarkDotNet.Jobs.RuntimeMoniker.Mono)]
+[BenchmarkDotNet.Attributes.SimpleJob(BenchmarkDotNet.Jobs.RuntimeMoniker.Net60)]
+[BenchmarkDotNet.Attributes.SimpleJob(BenchmarkDotNet.Jobs.RuntimeMoniker.Net70)]
 public class Test
 {
 	Random _Random = new Random();
