31ffca08272c567312d605266c17266126c66fdc
WooYoung <wymoon2690@gmail.com>
2022-11-25 01:47:54 +0900

    ㅈ

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Attributes;
+using System.Linq;
+using System.Security.Cryptography;
+
 Console.WriteLine("Hello, World!");
 var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Test>();
 
@@ -7,11 +11,72 @@ var summary = BenchmarkDotNet.Running.BenchmarkRunner.Run<Test>();
 [BenchmarkDotNet.Attributes.SimpleJob(BenchmarkDotNet.Jobs.RuntimeMoniker.Net70)]
 public class Test
 {
-	Random _Random = new Random();
+    private int[] data;
 
-	[BenchmarkDotNet.Attributes.Benchmark]
+    [Params(1000, 10000)]
+    public int N;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        var _Buffer = new byte[N];
+        new Random(42).NextBytes(_Buffer);
+
+        int[] bytesAsInts = Array.ConvertAll(_Buffer, c => (int)c);
+    }
+
+    [BenchmarkDotNet.Attributes.Benchmark]
 	public double MethodIf()
 	{
 		return double.MaxValue % 2;
 	}
+
+    [BenchmarkDotNet.Attributes.Benchmark]
+    public double Linq()
+    {
+        return data.Sum();
+    }
+
+
+    [BenchmarkDotNet.Attributes.Benchmark]
+    public double LinqAsParallel()
+    {
+        return data.AsParallel().Sum();
+    }
+
+    [BenchmarkDotNet.Attributes.Benchmark]
+    public double For()
+    {
+        double d = 0;
+        for (int i = 0; i < data.Length; i++)
+        {
+            d += data[i];
+        }
+
+        return d;
+    }
+
+    [BenchmarkDotNet.Attributes.Benchmark]
+    public double ForEach()
+    {
+        double d = 0;
+        foreach (var item in data)
+        {
+            d += item;
+        }
+
+        return d;
+    }
+
+    [BenchmarkDotNet.Attributes.Benchmark]
+    public double ForEachAsParallel()
+    {
+        double d = 0;
+        foreach (var item in data.AsParallel())
+        {
+            d += item;
+        }
+
+        return d;
+    }
 }
